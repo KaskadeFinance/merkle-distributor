115acc7f2c8fb8bb114df17a0509f21c8ad91b84
rohit-hyp <rohit.verma@hypotenuse.ca>
2024-02-20 08:56:44 +0000

    Updating script to get trades between dates, generate and save merkle tree to supabase
    Updating script to get trades between dates, generate and save merkle tree to supabase

--- src/generateMerkleRoot.js
+++ src/generateMerkleRoot.js
@@ -1,30 +1,96 @@
 // Import necessary modules
 const fs = require('fs');
 const { parseBalanceMap } = require('./parse-balance-map');
+const Web3 = require('web3');
 
-// Example balances
-const balances = {
-    '0xFeCD3485B88Aa711156db8bb476f2530ceb9400D': 100000000000000000000,
-    '0x987AB8C9161d4063152DdBf93A2249BE66E12C36': 200000000000000000000
-}
+const { createClient } = require('@supabase/supabase-js');
+const supabaseKey = process.env.SUPABASE_KEY; //needs to be service key
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-// Function to generate Merkle Distributor Info and write it to a file
-async function generateMerkleDistributorInfo() {
-    try {
-        // Parse the balance map to get Merkle distributor info
-        const merkleDistributorInfo = parseBalanceMap(balances);
+const web3 = new Web3();
 
-        // Convert the information to a JSON string
-        const data = JSON.stringify(merkleDistributorInfo, null, 2);
+(async () => {
+    const campaignId = 1;
+    // Query Supabase trades table to get all trades between weeklyStartDate and weeklyEndDate
+    const getTradesBetweenDates = async () => {
+        const weeklyStartDate = "2024-02-05T00:00:00.000Z"; // Replace with current weeks start date?
+        const weeklyEndDate = "2024-02-19T00:00:00.000Z"; // Replace with current weeks end date?
+        try {
+            const { data, error } = await supabase
+                .from('trades')
+                .select('*')
+                .gte('created_at', weeklyStartDate)
+                .lte('created_at', weeklyEndDate);
+            if (error) {
+                throw new Error(error.message);
+            }
+            return data;
+        } catch (error) {
+            console.error('Error:', error);
+            return null;
+        }
+    };
 
-        // Write the JSON to a file
-        fs.writeFileSync('merkleDistributor.json', data);
-
-        console.log('Merkle distributor JSON file created.');
-    } catch (error) {
-        console.error('Error:', error);
+    // Call the function to get trades between dates
+    const trades = await getTradesBetweenDates();
+    // console.log('Trades:', trades);
+    const balances = {}
+    for (const trade of trades) {
+        // console.log(web3.utils)
+        if (trade.points_earned) {
+            console.log(trade.points_earned * 1e18);
+            console.log(web3.utils.toWei(trade.points_earned.toString()));
+            if (trade.wallet_id in balances) {
+                balances[trade.wallet_id] += trade.points_earned * 1e18
+            } else {
+                balances[trade.wallet_id] = trade.points_earned * 1e18
+            }
+        }
     }
-}
+    console.log(balances)
 
-// Run the function
-generateMerkleDistributorInfo();
+    // Function to generate Merkle Distributor Info and write it to a file
+    async function generateMerkleDistributorInfo() {
+        try {
+            // Parse the balance map to get Merkle distributor info
+            const merkleDistributorInfo = parseBalanceMap(balances);
+
+            // Convert the information to a JSON string
+            const data = JSON.stringify(merkleDistributorInfo, null, 2);
+
+
+
+            // Write the JSON to a file
+            fs.writeFileSync('merkleDistributor.json', data);
+
+            console.log('Merkle distributor JSON file created.');
+
+            // Function to insert data into weekly_merkles table
+            async function insertDataIntoWeeklyMerkles(data, merkleRoot) {
+                try {
+                    const { data: insertedData, error } = await supabase
+                        .from('weekly_merkles')
+                        .insert([{ merkle_tree: data, merkle_root: merkleRoot }]);
+                    console.log(insertedData)
+                    if (error) {
+                        throw new Error(error.message);
+                    }
+                    console.log('Data inserted into weekly_merkles table.');
+                } catch (error) {
+                    console.error('Error:', error);
+                }
+            }
+
+            // Call the function to insert data into weekly_merkles table
+            insertDataIntoWeeklyMerkles(merkleDistributorInfo, merkleDistributorInfo.merkleRoot);
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+
+    // Run the function
+    generateMerkleDistributorInfo();
+})();
